4bd3b85e434d2ee8fedda969bdd9cee55b940fd8
Mayte Muñoz <mmunozzros@gmail.com>
2026-04-19 15:43:28 -0600

    iluminacion añadida

--- LobelitoEntreLosMuertos/game/main.gd
+++ LobelitoEntreLosMuertos/game/main.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
+const LightTexture = preload("res://art/Light.png")
+const GRID_SIZE = 350
 
+@onready var fog = $Fog
+
+var display_width = ProjectSettings.get("display/window/size/viewport_width")
+var display_height = ProjectSettings.get("display/window/size/viewport_height")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
--- LobelitoEntreLosMuertos/lobelito.gd
+++ LobelitoEntreLosMuertos/lobelito.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
+	velocity = Vector2.ZERO
 	
 	if Input.is_action_pressed("derecha"):
 		animated_sprite.play("lobo_derecha")
