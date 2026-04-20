b9111c317931667c9a9c67624d13a157f29638b9
Mayte Muñoz <mmunozzros@gmail.com>
2026-04-20 02:00:54 -0600

    implementacion del objeto 'caseta', ahora es posible entrar a la caseta mediante el clic izquierdo

---
+++ LobelitoEntreLosMuertos/objects/caseta.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@export var next_scene_path: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			change_scene()
+			
+func change_scene():
+	get_tree().change_scene_to_file("res://game/inside.tscn")
+	
