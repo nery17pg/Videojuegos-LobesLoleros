3a3e5fa7c9a6ce1df7295703feccd2b455ec399c
knavarro777 <karolvampir@gmail.com>
2026-04-20 22:38:26 -0600

    encendidoluz

--- LobelitoEntreLosMuertos/game/luz.gd
+++ LobelitoEntreLosMuertos/game/luz.gd
@@ -1,4 +1,10 @@
 extends PointLight2D
 
+var luz_encendida = true
+
 func _physics_process(delta):
 	position = get_global_mouse_position()
+
+	if Input.is_action_just_pressed("toggle_light"):
+		luz_encendida = !luz_encendida
+		visible = luz_encendida
